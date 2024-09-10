8bbd1c31112f4a5595b21fe87d921e136777dadc
dngeonMasteer <kaushikborah4080@gmail.com>
2024-09-11 01:03:50 +0530

    Mobile Menu Logic

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -54,8 +54,22 @@ const Navbar = () => {
         className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
      }
 
+     <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon}/>
+     {/* {Mobile Menu} */}
+     <div className={`${showMenu?'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+        <div className='flex items-center justify-between px-5 py-6'>
+            <img className='w-36' src={assets.logo}/>
+            <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon}/>
+        </div>
+        <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <NavLink  onClick={()=>setShowMenu(false)} to={'/'}><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
+            <NavLink onClick={()=>setShowMenu(false)} to={'/doctors'}><p className='px-4 py-2 rounded inline-block'>Doctors</p></NavLink>
+            <NavLink onClick={()=>setShowMenu(false)} to={'/about'}><p className='px-4 py-2 rounded inline-block'>About Us</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to={'contact'}><p className='px-4 py-2 rounded inline-block'>Contact Us</p></NavLink>
+        </ul>
      </div>
     </div>
+</div>
   )
 }
 
